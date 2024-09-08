8116b11f60d5bedd588ecfaf852b254f0b71fd49
Chernikkaaa <mykhailo.chernik@lyceum27lutsk.org>
2024-09-08 16:15:50 +0300

    Внесено зміни після рецензування

--- MyScript.py
+++
@@ -1,5 +0,0 @@
-a = int(input('first number: '))
-b = int(input('second number: '))
-c = int(input('third number: '))
-summ = a + b + c
-print(f'Summ of numbers: {summ}')
--- my_scriipt.py
+++
@@ -1,4 +0,0 @@
-a = 2
-b = 5
-c = b - a
-print (c)
