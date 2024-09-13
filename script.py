261c2c1fa56fc38652b29c6819b8ea207227fb27
avdul2288 <antontart2007@gmail.com>
2024-09-13 12:02:00 +0300

    NewScript.py
    Ускладнили скрипт

--- script.py
+++ script.py
@@ -1,1 +1,19 @@
-print ("Hello worldd")
+a = input("Введіть дію : ")
+
+d = int(input("Введіть перше число:"))
+
+v = int(input("Введіть друге число:"))
+
+if a == ("+"):
+  f = (d + v)
+
+if a == ("-"):
+  f = (d - v)
+
+
+print(f)
+
+
+
+
+
