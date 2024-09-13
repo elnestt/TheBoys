4536d1d7f3cedd4b2b04c3a2fcca3f22deb6e846
avdul2288 <antontart2007@gmail.com>
2024-09-13 12:13:45 +0300

    Update_myskriptik.py
    Покращили код

--- myskriptik.py
+++ myskriptik.py
@@ -1,1 +1,14 @@
-print ("Hello Kostya!")
+import random
+n = random.randrange(1,10)
+guess = int(input("Введіть будь-яке число від 1 до 10: "))
+while n!= guess:
+    if guess < n:
+        print("Мало")
+        guess = int(input("Введіть число знову: "))
+    elif guess > n:
+        print("Багато")
+        guess = int(input("Введіть число знову: "))
+    else:
+      break
+print("вгадав!")
+
